736db6052dc58472a9967b871a643a8f8b6bb225
ClaireeW <engicwangjh@gmail.com>
2021-03-21 23:44:21 -0400

    Claire: updated start page

--- app/src/main/java/com/example/dog/MainActivity.java
+++ app/src/main/java/com/example/dog/MainActivity.java
@@ -2,21 +2,25 @@ package com.example.dog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    private ImageButton button;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.buttonStart);
+
+        button = (ImageButton) findViewById(R.id.imageButton2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
